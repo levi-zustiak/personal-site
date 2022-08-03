f18180a97fcc171b7c3980233b048e491866ec16
levi-zustiak <levizustiak@gmail.com>
2022-08-02 19:03:04 -0500

    [feat] add social links with animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,47 @@
-import { useSpring, animated } from "react-spring";
 import Head from 'next/head';
+import { useSpring, useTransition, useChain, useSpringRef, a } from "react-spring";
 import styles from '../styles/Home.module.scss';
 
 export default function Home() {
-  const style = useSpring({
-    to: { opacity: 1 },
-    from: { opacity: 0 },
+  const links = [
+    {
+      text: "linkedin",
+      icon: "",
+      url: "https://www.linkedin.com/in/levi-zustiak/",
+    },
+    {
+      text: "github",
+      icon: "",
+      url: "https://www.github.com/levi-zustiak",
+    },
+    {
+      text: 'resume',
+      icon: "",
+      url: "https://www.levizustiak.dev/resume"
+    }
+  ];
+
+  const headerRef = useSpringRef();
+  const linkRef = useSpringRef();
+
+  const headerAnimation = useSpring({
+    ref: headerRef,
+    from: { opacity: 0, y: 16 },
+    to: { opacity: 1, y: 0 },
     config: {
       tension: 100,
-      friction: 64,
-    }
+    },
   });
+
+  const linkTransition = useTransition(links, {
+    ref: linkRef,
+    trail: 250,
+    from: { opacity: 0, y: 10 },
+    enter: { opacity: 1, y: 0 },
+    delay: 250
+  })
+
+  useChain([headerRef, linkRef]);
 
   return (
     <div className={styles.app}>
@@ -22,7 +53,12 @@ export default function Home() {
       <meta name="keywords" content="software engineer, software developer, portfolio, react, javascript, web app"/>
     </Head>
       <div className={styles.container}>
-        <animated.h1 style={style}>Hello World</animated.h1>
+        <a.h1 style={headerAnimation}>Hello World</a.h1>
+        <div className={styles.linkContainer}>
+          {linkTransition((style, link) => (
+            <a.a className={styles[link.text]} style={style} href={link.url}>{link.text}</a.a>
+          ))}
+        </div>
       </div>
     </div>
   )
