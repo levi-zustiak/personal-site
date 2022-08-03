dac3d4fdd5be99ffaf85969c9c6e6abcc974dcac
levi-zustiak <levizustiak@gmail.com>
2022-08-02 19:02:28 -0500

    [feat] add meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,10 @@ export default function Home() {
     <div className={styles.app}>
     <Head>
       <link rel="icon" href="/favicon.svg"/>
+      <title>Levi Zustiak</title>
+      <meta name="description" content="Hi! I'm Levi Zustiak. A software engineer based in the midwestern United States. Feel free to stop by and say hello!"/>
+      <meta name="author" content="Levi Zustiak"/>
+      <meta name="keywords" content="software engineer, software developer, portfolio, react, javascript, web app"/>
     </Head>
       <div className={styles.container}>
         <animated.h1 style={style}>Hello World</animated.h1>
