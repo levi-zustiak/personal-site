dccf8069684a43375673689dc577e82f56d02793
levi-zustiak <levizustiak@gmail.com>
2022-08-02 19:00:53 -0500

    [styles] changes Home module to scss file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import { useSpring, animated } from "react-spring";
-import styles from '../styles/Home.module.css';
 import Head from 'next/head';
+import styles from '../styles/Home.module.scss';
 
 export default function Home() {
   const style = useSpring({
