a4b495a628a9455d60e111dafda058fffc8a8158
levi-zustiak <levizustiak@gmail.com>
2022-08-03 21:27:52 -0500

    [feat] moved next/head tag to _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,19 @@
+import Head from 'next/head';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.svg"/>
+        <title>Levi Zustiak</title>
+        <meta name="description" content="Hi! I'm Levi Zustiak. A software engineer based in the midwestern United States. Feel free to stop by and say hello!"/>
+        <meta name="author" content="Levi Zustiak"/>
+        <meta name="keywords" content="software engineer, software developer, portfolio, react, javascript, web app"/>
+      </Head>
+      <Component {...pageProps}/>
+    </>
+  )
 }
 
 export default MyApp
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import { useSpring, useTransition, useChain, useSpringRef, a } from "react-spring";
 import styles from '../styles/Home.module.scss';
 
@@ -44,13 +43,6 @@ export default function Home() {
 
   return (
     <div className={styles.app}>
-    <Head>
-      <link rel="icon" href="/favicon.svg"/>
-      <title>Levi Zustiak</title>
-      <meta name="description" content="Hi! I'm Levi Zustiak. A software engineer based in the midwestern United States. Feel free to stop by and say hello!"/>
-      <meta name="author" content="Levi Zustiak"/>
-      <meta name="keywords" content="software engineer, software developer, portfolio, react, javascript, web app"/>
-    </Head>
       <div className={styles.container}>
         <a.h1 style={headerAnimation}>Hello World</a.h1>
         <div className={styles.linkContainer}>
